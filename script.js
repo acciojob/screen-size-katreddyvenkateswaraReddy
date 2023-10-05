feb2d7432b4ab9610881447e40793f360e6ca1c9
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2023-10-05 11:42:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const divElement =  document.createElement("div");
+divElement.id = "sizeInfo"
+const sizeElement =  document.createElement("h1");
+sizeElement.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+
+divElement.append(sizeElement)
+document.body.append(divElement);
+
+window.onresize = function () {
+	sizeElement.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+}
